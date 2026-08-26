832e8ee99381b79fc982ada00228fadaee538259
Binnur KURT <deepcloudlabs@gmail.com>
2026-08-26 15:38:34 +0300

    Update LazyBusinessService.java

--- singleton-pattern/src/com/example/singleton/LazyBusinessService.java
+++ singleton-pattern/src/com/example/singleton/LazyBusinessService.java
@@ -3,18 +3,19 @@ package com.example.singleton;
 import static java.util.Objects.isNull;
 
 public class LazyBusinessService {
-    private static LazyBusinessService instance = null ;
+	private static LazyBusinessService instance = null;
 
-    private LazyBusinessService() {
-        System.err.println("LazyBusinessService()");
-    }
-    public void fun(){
-        System.err.println("Have fun...");
-    }
+	private LazyBusinessService() {
+		System.err.println("LazyBusinessService()");
+	}
 
-    public static LazyBusinessService getInstance() {
-        if (isNull(instance))
-            instance = new LazyBusinessService(); // Once
-        return instance;
-    }
+	public void fun() {
+		System.err.println("Have fun...");
+	}
+
+	public static LazyBusinessService getInstance() {
+		if (isNull(instance))
+			instance = new LazyBusinessService(); // Once
+		return instance;
+	}
 }
